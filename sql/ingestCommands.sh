1de45ed2e01e2cfeac59fd77994e99baf81115ff
James McManus <jmpmcman@renci.org>
2024-04-19 13:02:15 -0400

    Added shel script for ingest

---
+++ sql/ingestCommands.sh
@@ -0,0 +1,3 @@
+python ingestPfasSamples.py --inputDir /projects/pfas/data/ --inputFile serumSamples.csv
+python ingestPfasSamples.py --inputDir /projects/pfas/data/ --inputFile dustSamples.csv
+python ingestPfasSamples.py --inputDir /projects/pfas/data/ --inputFile waterSamples.csv
