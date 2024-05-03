2dcecff7e3f6f736e3bc8ed26b222f08b6edbf8a
James McManus <jmpmcman@renci.org>
2024-05-03 09:03:29 -0400

    Change zipcode dtype to varchar

--- sql/createLocation.sql
+++ sql/createLocation.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS podm_location (
     location_id SERIAL PRIMARY KEY,
     city VARCHAR(200),
     state VARCHAR(2),
-    zipcode NUMERIC(5,0),
+    zipcode VARCHAR(7),
     latitude NUMERIC,
     longitude NUMERIC
 );
